364a06bd9c41c71b18ee69391d9fafd8ba79c4f2
daadesina <daadesina1@gmail.com>
2024-05-21 11:01:36 +0100

    keenlight

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,21 @@ class BaseModel:
     '''
     a BaseModel class
     '''
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+
 
     def save(self):
         self.updated_at = datetime.utcnow()
